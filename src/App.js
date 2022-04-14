12cb16a574af8c3b5a98d99a74028e40eb3dead1
Davi Luna <davijluna@gmail.com>
2022-04-14 13:03:28 -0300

    requesito 3

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
 import Form from './components/Form';
+import Card from './components/Card';
 
 class App extends React.Component {
-  // constructor() {
-  //   super();
+  constructor() {
+    super();
 
-  //   this.state = {
-  //     cardName: '',
-  //     cardDescription: '',
-  //     cardAttr1: '',
-  //     cardAttr2: '',
-  //     cardAttr3: '',
-  //     cardImage: '',
-  //     cardRare: '',
-  //     cardTrunfo: false,
-  //     isSaveButtonDisabled: false,
-  //   };
-  // this.onInputChange = this.onInputChange(this);
-  // this.onSaveButtonClick = this.onSaveButtonClick(this);
-  // }
+    this.state = {
+      cardName: '',
+      cardDescription: '',
+      cardAttr1: '',
+      cardAttr2: '',
+      cardAttr3: '',
+      cardImage: '',
+      cardRare: '',
+      cardTrunfo: false,
+      isSaveButtonDisabled: false,
+    };
+    this.onInputChange = this.onInputChange(this);
+    this.onSaveButtonClick = this.onSaveButtonClick(this);
+  }
 
   onInputChange({ target }) {
     const { name, value } = target;
@@ -32,21 +33,22 @@ class App extends React.Component {
   }
 
   render() {
-    // const {
-    //   cardName,
-    //   cardDescription,
-    //   cardAttr1,
-    //   cardAttr2,
-    //   cardAttr3,
-    //   cardImage,
-    //   cardRare,
-    //   cardTrunfo,
-    //   isSaveButtonDisabled,
-    // } = this.state;
+    const {
+      cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+      cardRare,
+      cardTrunfo,
+      isSaveButtonDisabled,
+    } = this.state;
     return (
       <div>
         <h1>Tryunfo </h1>
         <Form />
+        <Card />
       </div>
     );
   }
---
+++ src/components/Card.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Card extends Component {
+  render() {
+    const {
+      cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+      cardRare,
+      cardTrunfo,
+    } = this.props;
+    return (
+      <div>
+        <h1 data-testid="name-card">{cardName}</h1>
+        <img src={ cardImage } alt={ cardName } data-testid="image-card" />
+        <h2 data-testid="description-card">{cardDescription}</h2>
+        <div data-testid="attr1-card">{cardAttr1}</div>
+        <div data-testid="attr2-card">{cardAttr2}</div>
+        <div data-testid="attr3-card">{cardAttr3}</div>
+        <div data-testid="rare-card">{cardRare}</div>
+        {cardTrunfo && <p data-testid="trunfo-card">Super Trunfo</p>}
+      </div>
+    );
+  }
+}
+
+Card.propTypes = {
+  cardName: PropTypes.string.isRequired,
+  cardDescription: PropTypes.string.isRequired,
+};
+
+export default Card;
